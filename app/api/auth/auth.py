2b9d562613e813e1c2e99ef3d77865826f38962c
Thimira Sadeesha <thimira@maximimpressions.com>
2026-01-16 14:12:46 +0530

    user reg fixed

--- app/api/auth/auth.py
+++ app/api/auth/auth.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException
-
+from fastapi import APIRouter, HTTPException, status
+from gotrue.errors import AuthApiError
 from app.core.supabase.supabase_client import get_db
-
 from pydantic import BaseModel
 import logging
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
@@ -20,30 +20,32 @@ class LoginRequest(BaseModel):
     password: str
 
 
-@router.post("/register")
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 def register_user(data: RegisterRequest):
     supabase = get_db()
-
-    logger.info(f"Registering user: {data.email}")
-
-    # 1. Create Supabase auth user
-    auth_response = supabase.auth.sign_up({
-        "email": data.email,
-        "password": data.password,
-    })
+    try:
+        auth_response = supabase.auth.sign_up({
+            "email": data.email,
+            "password": data.password,
+        })
+    except AuthApiError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     if not auth_response.user:
         raise HTTPException(status_code=400, detail="Registration failed")
 
     user_id = auth_response.user.id
-
-    # 2. Insert profile
     supabase.table("tbl_users").insert({
         "id": user_id,
+        "hashed_password": data.password,
         "username": data.username,
+        "email": data.email,
     }).execute()
 
-    return {"message": "User registered successfully"}
+    return {
+        "status_code": "201",
+        "message": "User created successfully"
+    }
 
 
 @router.post("/login")
