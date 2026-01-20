26320ea5197300152afb6da5cf8e7cbeeb32926d
Thimira Sadeesha <thimira@maximimpressions.com>
2026-01-20 16:21:09 +0530

    refactor: simplify container management endpoints by removing unused request model

--- app/api/routes/docker.py
+++ app/api/routes/docker.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Header
 from pydantic import BaseModel
 import logging
-
 from app.services.docker_service import get_docker_stats, docker_start_container, docker_stop_container, \
     docker_restart_container
 from app.services.server_service import get_client
@@ -25,36 +24,37 @@ def docker_stats():
         "data": get_docker_stats(client)
     }
 
-
 @router.post("/start")
-def start_container(request: DockerActionRequest):
+def start_container(container_name: str):
     client = get_client()
     if client is None:
         raise HTTPException(status_code=400, detail="SSH not connected. Please call /ssh first.")
+
     return {
         "status": "success",
-        "data": docker_start_container(client, request.container_name)
+        "data": docker_start_container(client, container_name)
     }
 
 
 @router.post("/stop")
-def stop_container(request: DockerActionRequest):
+def stop_container(container_name: str):
     client = get_client()
     if client is None:
         raise HTTPException(status_code=400, detail="SSH not connected. Please call /ssh first.")
 
     return {
         "status": "success",
-        "data": docker_stop_container(client, request.container_name)
+        "data": docker_stop_container(client, container_name)
     }
 
 
 @router.post("/restart")
-def restart_container(request: DockerActionRequest):
+def restart_container(container_name: str):
     client = get_client()
     if client is None:
         raise HTTPException(status_code=400, detail="SSH not connected. Please call /ssh first.")
+
     return {
         "status": "success",
-        "data": docker_restart_container(client, request.container_name)
+        "data": docker_restart_container(client, container_name)
     }
